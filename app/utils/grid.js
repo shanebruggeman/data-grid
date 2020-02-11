1c8c78f341298883978411fb1a35755bb46a4537
Shane S Bruggeman <shanebruggeman@rose-hulman.edu>
2020-02-10 21:58:48 -0500

    tests for grid object (column, row, cell) intersection

--- app/utils/grid.js
+++ app/utils/grid.js
@@ -1,29 +1,60 @@
 export class StandardCell {
-  constructor(value) {
-      this._value = value;
-  }
+    constructor(value) {
+        this._value = value;
+    }
 
-  get value() {
-      return this._value;
-  }
+    intersect(gridObject) {
+        gridObject = gridObject || {};
+
+        const otherCells = gridObject.cells || [gridObject];
+        return otherCells.find(cell => cell === this);
+    }
+
+    get value() {
+        return this._value;
+    }
 }
 
 export class StandardRow {
-  constructor(cells) {
-      this._cells = cells;
-  }
+    constructor(cells) {
+        this._cells = cells;
+    }
 
-  get cells() {
-      return this._cells;
-  }
+    intersect(gridObject) {
+        gridObject = gridObject || {};
+
+        const isCell = gridObject instanceof StandardCell;
+        const isColumn = gridObject instanceof StandardColumn;
+
+        if (isCell || isColumn) {
+            const otherCells = gridObject.cells || [gridObject];
+            return this.cells.find(c => otherCells.includes(c));
+        }
+    }
+
+    get cells() {
+        return this._cells;
+    }
 }
 
 export class StandardColumn {
-  constructor(cells) {
-      this._cells = cells;
-  }
+    constructor(cells) {
+        this._cells = cells;
+    }
 
-  get cells() {
-      return this._cells;
-  }
+    intersect(gridObject) {
+        gridObject = gridObject || {};
+
+        const isRow = gridObject instanceof StandardRow;
+        const isCell = gridObject instanceof StandardCell;
+
+        if (isRow || isCell) {
+            const otherCells = gridObject.cells || [gridObject];
+            return this.cells.find(c => otherCells.includes(c));
+        }
+    }
+
+    get cells() {
+        return this._cells;
+    }
 }
--- tests/unit/utils/grid-test.js
+++ tests/unit/utils/grid-test.js
@@ -10,6 +10,38 @@ module('Unit | Utility | grid', function() {
         assert.equal(standardCell.value, 'value 1', 'column has correct value');
     });
 
+    test('StandardCell | it finds intersections', function (assert) {
+        const testCell = new StandardCell('Test Cell');
+        const otherCell = new StandardCell('Other Cell');
+
+        // bad input
+        assert.notOk(testCell.intersect(), 'with no input');
+        assert.notOk(testCell.intersect(NaN), 'with NaN input');
+        assert.notOk(testCell.intersect(null), 'with null input');
+        assert.notOk(testCell.intersect(undefined), 'with undefined input');
+        assert.notOk(testCell.intersect([]), 'with array input');
+        assert.notOk(testCell.intersect({}), 'with empty object input');
+        assert.notOk(testCell.intersect(copy(testCell), 'with cell copy'));
+
+        // with cells
+        assert.ok(testCell.intersect(testCell, 'Cell intersects itself'));
+        assert.notOk(testCell.intersect(otherCell), 'Cell cannot intersect a different cell');
+
+        // with rows
+        const containingRow = new StandardRow([testCell]);
+        const nonContainingRow = new StandardRow([otherCell]);
+
+        assert.ok(testCell.intersect(containingRow), 'Cell intersects containing row');
+        assert.notOk(testCell.intersect(nonContainingRow), 'Cell does not intersect a non-containing row');
+
+        // with columns
+        const containingColumn = new StandardColumn([testCell]);
+        const nonContainingColumn = new StandardColumn([otherCell]);
+
+        assert.ok(testCell.intersect(containingColumn), 'Cell intersects containing column');
+        assert.notOk(testCell.intersect(nonContainingColumn), 'Cell does not intersect a non-containing column');
+    });
+
     test('StandardRow | it creates a grid row', function (assert) {
         const cells = cellsFor(['value 1', 'value 2']);
         const cellsCopy = copy(cells);
@@ -18,11 +50,75 @@ module('Unit | Utility | grid', function() {
         assert.deepEqual(rowCells.cells, cells, 'row has correct cells')
     });
 
+    test('StandardRow | it finds intersections', function (assert) {
+        const cell1 = new StandardCell('Cell 1');
+        const cell2 = new StandardCell('Cell 2');
+
+        const testRow = new StandardRow([cell1]);
+        const otherRow = new StandardRow([cell2]);
+
+        // bad input
+        assert.notOk(testRow.intersect(), 'with no input');
+        assert.notOk(testRow.intersect(NaN), 'with NaN input');
+        assert.notOk(testRow.intersect(null), 'with null input');
+        assert.notOk(testRow.intersect(undefined), 'with undefined input');
+        assert.notOk(testRow.intersect([]), 'with array input');
+        assert.notOk(testRow.intersect({}), 'with empty object input');
+        assert.notOk(testRow.intersect(copy(cell1)), 'with cell copy');
+
+        // with cells
+        assert.equal(testRow.intersect(cell1), cell1, 'Row intersects contained cell');
+        assert.notOk(testRow.intersect(cell2), 'Row does not intersect cell not in the row');
+
+        // with rows
+        assert.notOk(testRow.intersect(testRow), 'Row does not intersect itself');
+        assert.notOk(testRow.intersect(otherRow), 'Row does not intersect other row');
+
+        // with columns
+        const intersectingColumn = new StandardColumn([cell1]);
+        const nonIntersectingColumn = new StandardColumn([cell2]);
+
+        assert.equal(testRow.intersect(intersectingColumn), cell1, 'Row intersects column with the same cell');
+        assert.notOk(testRow.intersect(nonIntersectingColumn), 'Row does not intersect column with no intersecting cells');
+    });
+
     test('StandardColumn | it creates a grid column', function (assert) {
         const cells = cellsFor(['column-1', 'column-2']);
         const cellsCopy = copy(cells);
 
         const columnCells = new StandardColumn(cellsCopy);
         assert.deepEqual(columnCells.cells, cells, 'column has correct cells');
+    });
+
+    test('StandardColumn | it finds intersections', function (assert) {
+        const cell1 = new StandardCell('Cell 1');
+        const cell2 = new StandardCell('Cell 2');
+
+        const testColumn = new StandardColumn([cell1]);
+        const otherColumn = new StandardColumn([cell2]);
+
+        // bad input
+        assert.notOk(testColumn.intersect(), 'with no input');
+        assert.notOk(testColumn.intersect(NaN), 'with NaN input');
+        assert.notOk(testColumn.intersect(null), 'with null input');
+        assert.notOk(testColumn.intersect(undefined), 'with undefined input');
+        assert.notOk(testColumn.intersect([]), 'with array input');
+        assert.notOk(testColumn.intersect({}), 'with empty object input');
+        assert.notOk(testColumn.intersect(copy(cell1)), 'with cell copy');
+
+        // with cells
+        assert.equal(testColumn.intersect(cell1), cell1, 'Column intersects contained cell');
+        assert.notOk(testColumn.intersect(cell2), 'Column does not intersect cell not in the row');
+
+        // with rows
+        const intersectingRow = new StandardRow([cell1]);
+        const nonIntersectingRow = new StandardRow([cell2]);
+
+        assert.equal(testColumn.intersect(intersectingRow), cell1, 'Column intersects row with the same cell');
+        assert.notOk(testColumn.intersect(nonIntersectingRow), 'Column does not intersect row with no intersecting cells');
+
+        // with columns
+        assert.notOk(testColumn.intersect(testColumn), 'Column does not intersect itself');
+        assert.notOk(testColumn.intersect(otherColumn), 'Column does not intersect other column');
     });
 });
