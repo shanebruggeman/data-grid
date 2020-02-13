f2176eb0f022ed8a2c1f12d99c098e581dc3f3a5
Shane S Bruggeman <shanebruggeman@rose-hulman.edu>
2020-02-13 17:32:10 -0500

    test changing css custom variable from glimmer component

--- app/components/data-grid/component.js
+++ app/components/data-grid/component.js
@@ -1,5 +1,21 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-export default Component.extend({
-    classNames: ['data-grid']
-});
+export default class DataGridComponent extends Component {
+    constructor() {
+        super(...arguments);
+
+        this.cells = [];
+        this.columnCount = 1;
+
+        for (let i = 0; i < 100; i++) {
+            this.cells.push({ id: i, value: i });
+        }
+    }
+
+    @action changeData() {
+        this.columnCount++;
+        document.querySelector('.data-grid').style.setProperty('--table-columns', this.columnCount);
+    }
+}
+
